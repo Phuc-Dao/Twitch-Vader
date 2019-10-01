6722538eee3b2215e5cb5ea8e307c2c10d9fbc79
Phuc-Dao <pcdao@ucsd.edu>
2019-09-30 23:55:19 -0700

    added inital socket connection and settings.py

---
+++ Settings.py
@@ -0,0 +1,19 @@
+"""
+Created on Oct 11, 2016
+@author: bthom
+"""
+
+#HOST is directory to connect to
+HOST = "irc.chat.twitch.tv"
+#PORT is default for Twitch.tv
+PORT = 6667
+#PASS is unique bot authentication, this is from benbottest
+PASS = "oauth:1ttapsvt9h0dpxifp5v8chzr7q263a"
+#name of bot, cannot change
+USER = "benbottest"
+#twich channel connecting to
+CHANNEL = "channelname"
+#maximum send rate of bot
+RATE = (20/30) #messages per second
+#volume of message cache for analysis
+CACHE = 1
